320b3c3744cbe984003fb6297d62e7f67684ede5
Zhiker <josemanuelmartinezduran@gmail.com>
2023-01-23 09:52:10 -0600

    Angular project start

---
+++ src/app/templates/polaris/polaris.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-polaris',
+  templateUrl: './polaris.component.html',
+  styleUrls: ['./polaris.component.css']
+})
+export class PolarisComponent {
+
+}
